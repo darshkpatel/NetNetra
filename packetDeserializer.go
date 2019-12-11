563b2362fe99b1b67147f165d86d77e5f3599fff
Darsh Patel <darshkpatel@gmail.com>
2019-12-11 22:56:19 +0530

    Add deserializer and elastic handler scaffolding

---
+++ packetDeserializer.go
@@ -0,0 +1,12 @@
+package main
+
+// deserializePacket Will contain parameters to index on elasticsearch
+type deserializedPacket struct {
+}
+
+// takes DecodedPacket and returns a deserializedPacket with the necessary fields to index on elasticsearch
+func deserializePacket(decodedpacket DecodedPacket) deserializedPacket {
+	deserializedpacket := deserializedPacket{}
+
+	return deserializedpacket
+}
