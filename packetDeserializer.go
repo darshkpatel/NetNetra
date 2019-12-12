1d7d7f96896b6dd703243533243cc22269873e7c
Darsh Patel <darshkpatel@gmail.com>
2019-12-12 23:05:20 +0530

    Proposed Data to Store for Deserialized Packet

--- packetDeserializer.go
+++ packetDeserializer.go
@@ -2,6 +2,21 @@ package main
 
 // deserializePacket Will contain parameters to index on elasticsearch
 type deserializedPacket struct {
+	FlowHash      uint64
+	NetFlow       string
+	Payload       string
+	IPv4Length    uint32
+	TCPpacketType string
+	SrcPort       uint32
+	DstPort       uint32
+	DstMAC        string
+	SrcMAC        string
+	ttl           uint32
+	srcIP         string
+	dstIP         string
+	dnsType       string
+	dnsRespCode   uint64
+	dnsContent    string
 }
 
 // takes DecodedPacket and returns a deserializedPacket with the necessary fields to index on elasticsearch
